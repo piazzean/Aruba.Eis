6edc1f27d20932cf7de6983493510d79ec3ad1a8
Andrea Piazzesi <andrea.piazzesi@staff.aruba.it>
2018-01-03 18:19:34 +0100

    HtmlItems

---
+++ Aruba.Eis/Helpers/HtmlItems.cs
@@ -0,0 +1,32 @@
+﻿using Aruba.Eis.Context;
+using Aruba.Eis.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aruba.Eis.Helpers
+{
+    public static class HtmlItems
+    {
+        public static List<SelectListItem> RoleListItems { get; set; }
+
+        public static async Task LoadRoleListItems()
+        {
+            RoleListItems = new List<SelectListItem>();
+            var roleService = BeanFactory.GetInstance<IUserService>();
+            var roles = await roleService.SearchRoles();
+            foreach (var role in roles)
+            {
+                RoleListItems.Add(new SelectListItem()
+                    {
+                        Text = "Role AU",
+                        Value = "AU"
+                    }
+                );
+            }
+        }
+    }
+}
