ad7f1914cc044f857f2e861406dc9a177dcac754
Andrea Piazzesi <andrea.piazzesi@staff.aruba.it>
2018-01-05 07:55:07 +0100

    Schedule editing

---
+++ Aruba.Eis/Models/Views/ScheduleCreateViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Aruba.Eis.Models.Bl;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Aruba.Eis.Models.Views
+{
+    public class ScheduleCreateViewModel
+    {
+        [Required]
+        public string Activity { get; set; }
+    }
+}
